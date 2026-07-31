7242903ea72cbde0b5061f7dd02d614ae782b5bd
lanangwisana <lanangwisana@gmail.com>
2026-07-31 14:47:52 +0700

    feat(migration): insert attribute in table about section

--- database/migrations/2026_07_31_073450_create_cms_about_sections_table.php
+++ database/migrations/2026_07_31_073450_create_cms_about_sections_table.php
@@ -13,6 +13,22 @@ return new class extends Migration
     {
         Schema::create('cms_about_sections', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->default('Tentang BSEC');
+            $table->string('subtitle');
+            $table->text('description_paragraph_1');
+            $table->text('description_paragraph_2')->nullable();
+            $table->text('vision_text');
+            $table->json('missions'); // Menyimpan array daftar misi
+            
+            //Stat Cards
+            $table->string('stat_card_1_number')->default('10+');
+            $table->string('stat_card_1_label')->default('TAHUN PENGALAMAN');
+            $table->string('stat_card_2_number')->default('500+');
+            $table->string('stat_card_2_label')->default('SISWA BERPRESTASI');
+            $table->string('stat_card_3_number')->default('95%');
+            $table->string('stat_card_3_label')->default('KEPUASAN SISWA');
+            $table->string('stat_card_4_number')->default('2014');
+            $table->string('stat_card_4_label')->default('TAHUN BERDIRI');
             $table->timestamps();
         });
     }
