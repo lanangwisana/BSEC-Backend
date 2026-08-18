63c0315a04c41cc129bb73ea93e9d94ebb64d0ce
lanangwisana <lanangwisana@gmail.com>
2026-08-18 11:21:27 +0800

    feat(routes):  register POST /api/admin/cms/upload-avatar-image route

--- routes/api.php
+++ routes/api.php
@@ -11,3 +11,4 @@ Route::get('/landing-page', [LandingPageController::class, 'index']);
 Route::get('/admin/cms/sections', [AdminCmsController::class, 'sections']);
 Route::post('/admin/cms/publish', [AdminCmsController::class, 'publish']);
 Route::post('/admin/cms/upload-hero-image', [AdminCmsController::class, 'uploadHeroImage']);
+Route::post('/admin/cms/upload-avatar-image', [AdminCmsController::class, 'uploadAvatarImage']);
