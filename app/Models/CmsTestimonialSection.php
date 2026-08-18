591437406f8021cdc428283d0ef1759cab826f90
lanangwisana <lanangwisana@gmail.com>
2026-08-18 11:23:44 +0800

    feat(model): create CmsTestimonialSection Eloquent model

---
+++ app/Models/CmsTestimonialSection.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CmsTestimonialSection extends Model
+{
+    protected $table = 'cms_testimonial_sections';
+
+    protected $fillable = [
+        'title',
+    ];
+}
