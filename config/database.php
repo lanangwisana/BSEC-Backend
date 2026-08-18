385259959fddecd08b3e30182b8eaa9aa14eddbf
lanangwisana <lanangwisana@gmail.com>
2026-08-18 15:10:17 +0800

    fix(cors): allow all origins in config/cors.php for Vercel production frontend

--- config/database.php
+++ config/database.php
@@ -87,7 +87,9 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_ENDPOINT')
+                ? env('DB_HOST') . " options='endpoint=" . env('DB_ENDPOINT') . "'"
+                : env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
